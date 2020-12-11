f5c5b6737a75c991bbc8ca7e9341e9d2c864b70a
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2020-12-12 02:05:23 +0530

    update

--- NotepadPP/target/[flatpak-wine]Notepadpp/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Installs game bundle for user.
-# You can delete everything after installation.
-
-DIR=$(dirname "$0")
-set -ex
-flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo || true
-flatpak --user install -y --app --bundle "$DIR/Notepadpp.flatpak" || echo "Installation failed. Check if you have Flatpak properly configured. See http://flatpak.org/ for more info."
--- NotepadPP/target/[flatpak-wine]Notepadpp/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# You can as well use package manager to uninstall the game
-echo You can as well use package manager to uninstall the game
-
-set -ex
-flatpak --user uninstall org.flatpakwine.Notepadpp
